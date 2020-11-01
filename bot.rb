0dfa9722b8e937d34d0ef99d2f8f9f6ec1ef4f58
Jiří Vrba <jiri@vrba.dev>
2020-11-01 13:16:29 +0100

    Delete messages after 2 minutes

--- bot.rb
+++ bot.rb
@@ -18,36 +18,49 @@ module Astronomia
         if arguments.empty?
           horoscope = api.horoscope_for_user event.author.id
 
-        # +horoscope register
+          # +horoscope register
         elsif arguments[0] == "register"
           raise Errors::InvalidZodiacSignError unless Horoscope.is_valid_zodiac? arguments[1]
 
           horoscope = api.register_user event.author.id, arguments[1]
 
-        # +horoscope aries
+          # +horoscope aries
         elsif arguments.length == 1
           raise Errors::InvalidZodiacSignError unless Horoscope.is_valid_zodiac? arguments[0]
 
           horoscope = api.horoscope_for_zodiac_sign arguments[0]
 
-        # Invalid usage
+          # Invalid usage
         else
           raise Errors::InvalidArgumentError
         end
 
         embed = horoscope.to_embed api
-        embed.footer = Discordrb::Webhooks::EmbedFooter.new text: event.message.user.username,
-                                                            icon_url: event.message.user.avatar_url
-
-        event.channel.send_message(nil, nil, embed)
-
       rescue StandardError => error
         embed = Discordrb::Webhooks::Embed.new title: "Ah snap!",
                                                description: error.message,
                                                color: "#ff6b6b"
 
         embed.add_field name: "Error type", value: "`#{error.class.to_s}`"
-        event.channel.send_message(nil, nil, embed)
+      end
+
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new text: event.message.user.username,
+                                                          icon_url: event.message.user.avatar_url
+
+      embed.timestamp = event.timestamp
+
+      message = event.channel.send_message(nil, nil, embed)
+
+      # Delete the message after 2 minutes
+      Thread.new do
+        sleep 2 * 60
+        message.delete
+
+        # And also delete the +horoscope if possible
+        begin
+          event.message.delete
+        rescue Discordrb::Errors::NoPermission => _
+        end
       end
     end
   end
