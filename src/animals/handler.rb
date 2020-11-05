1d1a76ddf8c45a15d52c4bd4c9c19ab8e461dda0
Jiří Vrba <jiri@vrba.dev>
2020-11-05 18:54:30 +0100

    Added pandas, birbs, koalas

--- src/animals/handler.rb
+++ src/animals/handler.rb
@@ -14,28 +14,63 @@ module SimpBot
                   }
               },
               title: "Meow!",
-              extract: -> (json) do json[0]["url"] end
+              extract: -> (json) do
+                json[0]["url"]
+              end
           },
           {
               commands: ["+dog", "+doggo"],
               url: "https://random.dog/woof.json",
               headers: {},
               title: "Woof!",
-              extract: -> (json) do json["url"] end
+              extract: -> (json) do
+                json["url"]
+              end
           },
           {
               commands: ["+fox", "+foxxo"],
               url: "https://randomfox.ca/floof/",
               headers: {},
               title: "What does the fox say?",
-              extract: -> (json) do json["image"] end
+              extract: -> (json) do
+                json["image"]
+              end
           },
           {
               commands: ["+duck", "+quack"],
               url: "https://random-d.uk/api/v1/random?type=png",
               headers: {},
               title: "Quack!",
-              extract: -> (json) do json["url"] end
+              extract: -> (json) do
+                json["url"]
+              end
+          },
+          {
+              commands: ["+panda"],
+              url: "https://some-random-api.ml/img/panda",
+              headers: {},
+              title: "Here, have a panda!",
+              extract: -> (json) do
+                json["link"]
+              end
+          },
+          {
+              commands: ["+bird", "+birb"],
+              url: "https://some-random-api.ml/img/birb",
+              headers: {},
+              title: "Tweet!",
+              extract: -> (json) do
+                json["link"]
+              end
+          },
+          {
+              commands: ["+koala"],
+              url: "https://some-random-api.ml/img/koala",
+              headers: {},
+              title: "A wild koala appears!",
+              extract: -> (json) do
+                json["link"]
+              end
           }
       ]
 
@@ -44,7 +79,9 @@ module SimpBot
       end
 
       def handle_message(event)
-        endpoint = ENDPOINTS.find do |endpoint| endpoint[:commands].include? event.message.content end
+        endpoint = ENDPOINTS.find do |endpoint|
+          endpoint[:commands].include? event.message.content
+        end
 
         unless endpoint.nil?
           response = HTTParty.get(endpoint[:url], headers: endpoint[:headers] || []).parsed_response
@@ -57,7 +94,9 @@ module SimpBot
           embed.footer = Discordrb::Webhooks::EmbedFooter.new text: event.message.user.username,
                                                               icon_url: event.message.user.avatar_url
 
-          if [".mp4", ".webm"].any? do |format| image.end_with? format end
+          if [".mp4", ".webm"].any? do |format|
+            image.end_with? format
+          end
             embed.description = image
           else
             embed.image = Discordrb::Webhooks::EmbedImage.new url: image
