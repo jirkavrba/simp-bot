8986871df1243d89fd757b3b6ab915421f82c29d
Jiří Vrba <jiri@vrba.dev>
2020-11-05 15:04:40 +0100

    Hey google, how to speak german

--- src/facts/api.rb
+++ src/facts/api.rb
@@ -1,7 +1,7 @@
 module SimpBot
   module Facts
     class Api
-      API_URL = "https://uselessfacts.jsph.pl/random.json"
+      API_URL = "https://uselessfacts.jsph.pl/random.json?language=en"
 
       def random_fact
         HTTParty.get(API_URL).parsed_response
