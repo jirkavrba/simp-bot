27ad5199750df6c72a79712c7f76e095a339dd4a
Jiří Vrba <jiri@vrba.dev>
2020-11-06 10:41:46 +0100

    Extracted endpoints to a separate file

---
+++ src/animals/endpoints.rb
@@ -0,0 +1,60 @@
+module SimpBot
+  module Animals
+    ENDPOINTS = [
+      {
+        commands: %w[+cat +catto +pussy],
+        url: "https://api.thecatapi.com/v1/images/search",
+        headers: {
+          "x-api-key" => ENV.fetch("CATS_API_KEY") {
+                           raise Animals::Errors::ApiTokenNotSetup.new "CATS_API_KEY"
+                         }
+        },
+        title: "Meow!",
+        extract: ->(json) { json[0]["url"] }
+      },
+      {
+        commands: %w[+dog +doggo],
+        url: "https://random.dog/woof.json",
+        title: "Woof!",
+        extract: ->(json) { json["url"] }
+      },
+      {
+        commands: %w[+fox +foxxo],
+        url: "https://randomfox.ca/floof/",
+        title: "What does the fox say?",
+        extract: ->(json) { json["image"] }
+      },
+      {
+        commands: %w[+duck +quack],
+        url: "https://random-d.uk/api/v1/random?type=png",
+        title: "Quack!",
+        extract: ->(json) { json["url"] }
+      },
+      {
+        commands: ["+panda"],
+        url: "https://some-random-api.ml/img/panda",
+        title: "Here, have a panda!",
+        extract: ->(json) { json["link"] }
+      },
+      {
+        commands: %w[+bird +birb],
+        url: "https://some-random-api.ml/img/birb",
+        title: "Tweet!",
+        extract: ->(json) { json["link"] }
+      },
+      {
+        commands: ["+koala"],
+        url: "https://some-random-api.ml/img/koala",
+        title: "A wild koala appears!",
+        extract: ->(json) { json["link"] }
+      },
+      {
+        commands: ["+sloth"],
+        url: "https://sloth.pics/api",
+        headers: {},
+        title: "Man, this API is kinda sloooow",
+        extract: ->(json) { json["url"] }
+      },
+    ]
+  end
+end
--- src/animals/handler.rb
+++ src/animals/handler.rb
@@ -3,80 +3,18 @@ require_relative "../handler"
 module SimpBot
   module Animals
     class MessageHandler < MessageHandlerBase
-      ENDPOINTS = [
-        {
-          commands: %w[+cat +catto +pussy],
-          url: "https://api.thecatapi.com/v1/images/search",
-          headers: {
-            "x-api-key" => ENV.fetch("CATS_API_KEY") {
-              raise Animals::Errors::ApiTokenNotSetup.new "CATS_API_KEY"
-            }
-          },
-          title: "Meow!",
-          extract: -> json { json[0]["url"] }
-        },
-        {
-          commands: %w[+dog +doggo],
-          url: "https://random.dog/woof.json",
-          headers: {},
-          title: "Woof!",
-          extract: -> json { json["url"] }
-        },
-        {
-          commands: %w[+fox +foxxo],
-          url: "https://randomfox.ca/floof/",
-          headers: {},
-          title: "What does the fox say?",
-          extract: -> json { json["image"] }
-        },
-        {
-          commands: %w[+duck +quack],
-          url: "https://random-d.uk/api/v1/random?type=png",
-          headers: {},
-          title: "Quack!",
-          extract: -> json { json["url"] }
-        },
-        {
-          commands: ["+panda"],
-          url: "https://some-random-api.ml/img/panda",
-          headers: {},
-          title: "Here, have a panda!",
-          extract: -> json { json["link"] }
-        },
-        {
-          commands: %w[+bird +birb],
-          url: "https://some-random-api.ml/img/birb",
-          headers: {},
-          title: "Tweet!",
-          extract: -> json { json["link"] }
-        },
-        {
-          commands: ["+koala"],
-          url: "https://some-random-api.ml/img/koala",
-          headers: {},
-          title: "A wild koala appears!",
-          extract: -> json { json["link"] }
-        },
-        {
-            commands: ["+sloth"],
-            url: "https://sloth.pics/api",
-            headers: {},
-            title: "Man, this API is kinda sloooow",
-            extract: -> json { json["url"] }
-        }
-      ]
 
       def initialize(repository)
         super repository
       end
 
       def handle_message(event)
-        endpoint = ENDPOINTS.find { |endpoint|
+        endpoint = Animals::ENDPOINTS.find { |endpoint|
           endpoint[:commands].include? event.message.content
         }
 
         unless endpoint.nil?
-          response = HTTParty.get(endpoint[:url], headers: endpoint[:headers] || []).parsed_response
+          response = HTTParty.get(endpoint[:url], headers: endpoint[:headers] || {}).parsed_response
           image = endpoint[:extract].call(response)
 
           embed = Discordrb::Webhooks::Embed.new
