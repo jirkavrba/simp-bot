e2785600ec66d212b05eeba33f1328360f6283c6
Jiří Vrba <jiri@vrba.dev>
2020-11-06 10:32:42 +0100

    Added sloth, formatted lambdas

--- src/animals/handler.rb
+++ src/animals/handler.rb
@@ -13,63 +13,56 @@ module SimpBot
             }
           },
           title: "Meow!",
-          extract: ->(json) do
-                     json[0]["url"]
-                   end
+          extract: -> json { json[0]["url"] }
         },
         {
           commands: %w[+dog +doggo],
           url: "https://random.dog/woof.json",
           headers: {},
           title: "Woof!",
-          extract: ->(json) do
-                     json["url"]
-                   end
+          extract: -> json { json["url"] }
         },
         {
           commands: %w[+fox +foxxo],
           url: "https://randomfox.ca/floof/",
           headers: {},
           title: "What does the fox say?",
-          extract: ->(json) do
-                     json["image"]
-                   end
+          extract: -> json { json["image"] }
         },
         {
           commands: %w[+duck +quack],
           url: "https://random-d.uk/api/v1/random?type=png",
           headers: {},
           title: "Quack!",
-          extract: ->(json) do
-                     json["url"]
-                   end
+          extract: -> json { json["url"] }
         },
         {
           commands: ["+panda"],
           url: "https://some-random-api.ml/img/panda",
           headers: {},
           title: "Here, have a panda!",
-          extract: ->(json) do
-                     json["link"]
-                   end
+          extract: -> json { json["link"] }
         },
         {
           commands: %w[+bird +birb],
           url: "https://some-random-api.ml/img/birb",
           headers: {},
           title: "Tweet!",
-          extract: ->(json) do
-                     json["link"]
-                   end
+          extract: -> json { json["link"] }
         },
         {
           commands: ["+koala"],
           url: "https://some-random-api.ml/img/koala",
           headers: {},
           title: "A wild koala appears!",
-          extract: ->(json) do
-                     json["link"]
-                   end
+          extract: -> json { json["link"] }
+        },
+        {
+            commands: ["+sloth"],
+            url: "https://sloth.pics/api",
+            headers: {},
+            title: "Man, this API is kinda sloooow",
+            extract: -> json { json["url"] }
         }
       ]
 
