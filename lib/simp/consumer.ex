8546f12d510855db794c579ffb0f0d852466e4f8
Jiří Vrba <jiri@vrba.dev>
2021-05-04 18:19:55 +0200

    Add commands scaffolding

--- lib/simp/consumer.ex
+++ lib/simp/consumer.ex
@@ -1,18 +1,31 @@
 defmodule Simp.Consumer do
   use Nostrum.Consumer
 
-  alias Nostrum.Api
+  alias Nosedrum.Invoker.Split, as: CommandInvoker
+  alias Nosedrum.Storage.ETS, as: CommandStorage
+
+  @commands %{}
+
+  @aliases %{}
 
   def start_link do
     Consumer.start_link(__MODULE__)
   end
 
+  def handle_event({:READY, _data, _state}) do
+    Enum.each(@commands, &register_command/1)
+  end
+
   def handle_event({:MESSAGE_CREATE, message, _state}) do
-    case message.content do
-      "ping!" -> Api.create_message(message.channel_id, "Pong!")
-      _ -> :ignore
-    end
+    CommandInvoker.handle_message(message, CommandStorage)
   end
 
   def handle_event(_event), do: :noop
+
+  defp register_command({name, command}) do
+    # Apply all specified aliases
+    names = [name] ++ Map.get(@aliases, name, [])
+
+    CommandStorage.add_command(names , command)
+  end
 end
