634cb5a51131ad4a8e6e3d9470e822dafaa9a46b
Jiří Vrba <jiri@vrba.dev>
2022-01-19 22:55:42 +0100

    Define ImageApiEndpoint properties

--- Services/ImageApiEndpoint.cs
+++ Services/ImageApiEndpoint.cs
@@ -1,6 +1,26 @@
 namespace SimpBot.Services;
 
-public class ImageApiEndpoint
+public abstract class ImageApiEndpoint
 {
+    /// <summary>
+    /// Name and aliases associated with this endpoint. (eg. [cat, catto, cattos, pussy, ...])
+    /// </summary>
+    public abstract IEnumerable<string> Names { get; init; }
+
+    /// <summary>
+    /// The API url endpoint
+    /// </summary>
+    public abstract string Url { get; init; }
+
+    /// <summary>
+    /// Title of the embed that is containing the image
+    /// </summary>
+    public abstract string Title { get; init; }
+
+    /// <summary>
+    /// HTTP headers that should be passed within the request (eg. Api keys)
+    /// </summary>
+    public abstract IDictionary<string, string> Headers { get; init; }
     
+    // TODO: Add methods for extracting image URL from HTTP response
 }
