6e56b4314424dfa09a7548048d8509927305d3f9
Jiří Vrba <jiri@vrba.dev>
2022-01-23 21:42:45 +0100

    Cache all queries allowing it

--- Commands/ManagementCommandsModule.cs
+++ Commands/ManagementCommandsModule.cs
@@ -1,6 +1,7 @@
 using System.Reflection.Metadata.Ecma335;
 using Discord;
 using Discord.Commands;
+using EFCoreSecondLevelCacheInterceptor;
 using Microsoft.EntityFrameworkCore;
 using SimpBot.Attributes;
 using SimpBot.Database;
@@ -128,7 +129,7 @@ public class ManagementCommandsModule : ModuleBase<SocketCommandContext>
 
         var flag = _flags[key];
         var guild = Context.Guild.Id;
-        var settings = await context.GuildSettings.FirstOrDefaultAsync(g => g.GuildId == guild)
+        var settings = await context.GuildSettings.Cacheable().FirstOrDefaultAsync(g => g.GuildId == guild)
                        ?? new GuildSettings {GuildId = guild};
 
         settings.EnabledFeatures &= ~flag;
